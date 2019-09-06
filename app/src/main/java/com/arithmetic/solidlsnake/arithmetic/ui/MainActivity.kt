6a7a6fe824ef26883262d4b05aa7b5d90b1df177
Vorkytaka <solidlsnake@yandex.ru>
2019-09-07 02:45:20 +0300

    28: • Initial commit

---
+++ app/src/main/java/com/arithmetic/solidlsnake/arithmetic/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.arithmetic.solidlsnake.arithmetic.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arithmetic.solidlsnake.arithmetic.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
